fe0ef4abae73d89fdd50d2ee74a029287166c415
fuszz <uliaszfilip@gmail.com>
2025-04-22 11:36:07 +0200

    Header & footer added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import ClassFinder from '../componetns/classExplorer/ClassFinder';
 import Timetable from '../componetns/timetable/Timetable';
+import Footer from '../componetns/footer/Footer';
+import Header from '../componetns/header/Header'
 import './Home.scss'
 export default function Home(){
     return (
+        <>
+        <Header/>
         <div className='home'>
             <Timetable/>
             {/* <ClassFinder/> */}
         </div>
+        <Footer/>
+        </>
     )
 }
