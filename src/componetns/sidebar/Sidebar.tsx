5859775a7979aff6a58e159c5131a43973aef6e7
fuszz <uliaszfilip@gmail.com>
2025-04-23 15:09:22 +0200

    Interactive sidebar

--- src/componetns/sidebar/Sidebar.tsx
+++ src/componetns/sidebar/Sidebar.tsx
@@ -1,6 +1,8 @@
 import {useState} from "react";
 import './Sidebar.scss'
 import SidebarButton from "./sidebarButton/SidebarButton.tsx";
+import SidebarBox from "./sidebarBox/SidebarBox.tsx";
+import { MdTripOrigin } from "react-icons/md";
 
 export default function Sidebar() {
     const [isOpen, setIsOpen] = useState(true);
@@ -8,13 +10,11 @@ export default function Sidebar() {
         <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
 
             <SidebarButton onClick={() => setIsOpen(prev => !prev)}/>
-            {isOpen && (
-                <ul>
-                    <li>Strona główna</li>
-                    <li>Profil</li>
-                    <li>Ustawienia</li>
-                </ul>
-            )}
+            <SidebarBox displayText={"Strona główna"} refLink={"https://umcs.pl"} Icon={MdTripOrigin} extended={isOpen}/>
+            <SidebarBox displayText={"O SKNI"} refLink={"https://umcs.pl"} Icon={MdTripOrigin} extended={isOpen}/>
+            <SidebarBox displayText={"Wydziały"} refLink={"https://umcs.pl"} Icon={MdTripOrigin} extended={isOpen}/>
+            <SidebarBox displayText={"Lista sal"} refLink={"https://umcs.pl"} Icon={MdTripOrigin} extended={isOpen}/>
+            <SidebarBox displayText={"Załóż konto"} refLink={"https://umcs.pl"} Icon={MdTripOrigin} extended={isOpen}/>
         </div>
     </>
 
