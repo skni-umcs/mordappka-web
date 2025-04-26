ce42b1d8b2ab1e9c54999fe7c7c7f9f298e77f94
Bartlomiej Dąbrowski <bartlomiej.dabrowski@skni.umcs.pl>
2025-04-26 10:08:59 +0200

    proxy config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
-    '/api': 'http://localhost:5000'
+    // '/api': 'http://localhost:5000',
+    '/api': 'http://10.50.50.77:8081',
     }
   }
 })
