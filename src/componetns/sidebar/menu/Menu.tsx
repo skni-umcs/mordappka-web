242d66fad4b46571b4dd4117a0eaa8d75281ae0e
cinis <bartlomiejdabrowski2000@gmail.com>
2025-04-29 11:57:18 +0200

    updated icons

--- src/componetns/sidebar/menu/Menu.tsx
+++ src/componetns/sidebar/menu/Menu.tsx
@@ -1,5 +1,5 @@
 import SidebarBox from "./../sidebarBox/SidebarBox.tsx";
-import { MdTripOrigin } from "react-icons/md";
+import { MdHome, MdInfo, MdSchool, MdMeetingRoom, MdPersonAdd } from "react-icons/md";
 
 interface Props {
     isOpen: boolean;
@@ -7,10 +7,10 @@ interface Props {
 
 export default function Menu({ isOpen } : Props) {
     return <div className="menu">
-        <SidebarBox displayText={"Strona główna"} refLink={"https://umcs.pl"} Icon={MdTripOrigin} extended={isOpen}/>
-        <SidebarBox displayText={"O SKNI"} refLink={"https://skni.umcs.pl/"} Icon={MdTripOrigin} extended={isOpen}/>
-        <SidebarBox displayText={"Wydziały"} refLink={"https://umcs.pl"} Icon={MdTripOrigin} extended={isOpen}/>
-        <SidebarBox displayText={"Lista sal"} refLink={"https://umcs.pl"} Icon={MdTripOrigin} extended={isOpen}/>
-        <SidebarBox displayText={"Załóż konto"} refLink={"https://umcs.pl"} Icon={MdTripOrigin} extended={isOpen}/>
+        <SidebarBox displayText={"Strona główna"} refLink={"https://umcs.pl"} Icon={MdHome} extended={isOpen}/>
+        <SidebarBox displayText={"Wydziały"} refLink={"https://umcs.pl"} Icon={MdSchool} extended={isOpen}/>
+        <SidebarBox displayText={"Lista sal"} refLink={"https://umcs.pl"} Icon={MdMeetingRoom} extended={isOpen}/>
+        <SidebarBox displayText={"Załóż konto"} refLink={"https://umcs.pl"} Icon={MdPersonAdd} extended={isOpen}/>
+        <SidebarBox displayText={"O SKNI"} refLink={"https://skni.umcs.pl/"} Icon={MdInfo} extended={isOpen}/>
     </div>
 }
--- src/componetns/sidebar/sidebarBox/SidebarBox.tsx
+++ src/componetns/sidebar/sidebarBox/SidebarBox.tsx
@@ -4,14 +4,14 @@ import './SidebarBox.scss'
 interface SidebarBox {
     displayText: string;
     refLink: string;
-    Icon: ComponentType;
+    Icon: ComponentType<{size?: number}>;
     extended: boolean;
 }
 
 export default function SidebarBox({displayText, refLink, Icon, extended}: SidebarBox) {
     return <div className={`sidebar-box ${extended ? 'open' : 'closed'}`}>
         <div className="icon">
-            <Icon />
+            <Icon size={38}/>
         </div>
         <div className="content">
             <a href={refLink} target="_blank" rel="noopener noreferrer">{displayText}</a>
